b9ab70a02de3d64ef4da475bd2aaf1b41d89c341
Isaac Kuri <kuri.embedded@gmail.com>
2022-12-28 17:04:15 -0600

    Tracking Issues

--- hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/isaac/Documents/repositories/rust-book/hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c: src/main.rs
-
-/mnt/c/Users/isaac/Documents/repositories/rust-book/hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/isaac/Documents/repositories/rust-book/hello_cargo/target/debug/hello_cargo: /mnt/c/Users/isaac/Documents/repositories/rust-book/hello_cargo/src/main.rs
